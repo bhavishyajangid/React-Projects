c36b087ae3ecf01580bb631d541ebaba9c87ab32
bhavishyajangid <bhavisya8824@gamil.com>
2024-09-20 18:00:10 +0530

    new

--- Quiz-app/src/assets/data.js
+++ Quiz-app/src/assets/data.js
@@ -1,59 +1,59 @@
 
  export const allQuestion = [
     {
-      askedQuestion: "1. what is your father name",
+      askedQuestion: "1. which is the national game of india",
       answer: [
-        { option1: "Bhavishya", correct: false },
-        { option2: "fut", correct: false },
-        { option3: "Bhavishya2", correct: true },
-        { option4: "Bhavishya3", correct: false },
+        { option1: "cricket", correct: false },
+        { option2: "chess", correct: false },
+        { option3: "hockey", correct: true },
+        { option4: "kabadi", correct: false },
       ],
 
     },
 
     {
-      askedQuestion: "2. what is your mother name",
+      askedQuestion: "2. which is the nationl bird of india",
       answer: [
-        { option1: "Bhavishya", correct: false },
-        { option2: "Future2", correct: true },
-        { option3: "Bhavishya2", correct: false },
-        { option4: "Bhavishya3", correct: false },
-      ],
-
-    },
-
-
-    {
-      askedQuestion: "3. what is your sister name",
-      answer: [
-        { option1: "Bhavishya", correct: true },
-        { option2: "Future3", correct: true },
-        { option3: "Bhavishya2", correct: false },
-        { option4: "Bhavishya3", correct: false },
+        { option1: "sparrow", correct: false },
+        { option2: "peacock", correct: true },
+        { option3: "eagle", correct: false },
+        { option4: "vulture", correct: false },
       ],
 
     },
 
 
     {
-      askedQuestion: "4. what is your brotehr name",
+      askedQuestion: "3. What is the capital of France ",
       answer: [
-        { option1: "Bhavishya", correct: false },
-        { option2: "Future4", correct: false },
-        { option3: "Bhavishya2", correct: false },
-        { option4: "Bhavishya3", correct: true },
+        { option1: "Paris", correct: true },
+        { option2: "Madrid", correct: false },
+        { option3: "Rome", correct: false },
+        { option4: " Berlin", correct: false },
       ],
 
     },
 
 
     {
-      askedQuestion: "5. what is your wife name",
+      askedQuestion: "4. Which is the largest mammal in the world",
       answer: [
-        { option1: "Bhavishya", correct: false },
-        { option2: "Futureeeeeeee", correct: false },
-        { option3: "Bhavishya2", correct: true },
-        { option4: "Bhavishya3", correct: false },
+        { option1: "African Elephant", correct: false },
+        { option2: "Great White Shark", correct: false },
+        { option3: "Giraffe", correct: false },
+        { option4: "Blue Whale", correct: true },
+      ],
+
+    },
+
+
+    {
+      askedQuestion: "5. In which year did the Titanic sink",
+      answer: [
+        { option1: "1905", correct: false },
+        { option2: "1935", correct: false },
+        { option3: "1912", correct: true },
+        { option4: "1920", correct: false },
       ],
 
     },
