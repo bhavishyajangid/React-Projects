07ec5045cda490cf7e4b09aff9cf7878e40a4cf9
bhavishyajangid <bhavisya8824@gamil.com>
2024-10-15 14:26:31 +0530

    hg

--- megaBlog-Project/src/component/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-const Logo = ({width , height}) => {
-  return <img className={`${width} ${height}`} src="https://i.pinimg.com/736x/bf/9c/78/bf9c788e68117921a42e8a6c671b4165.jpg" alt="" />
-}
-
-export default Logo
--- megaBlog-Project/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    appwriteEndPoint : String(import.meta.env.VITE_APPWRITE_END_POINT) , 
-    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appWriteDataBaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appWriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
-
-export default config
